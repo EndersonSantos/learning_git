c206928749b1687e2c28601aaf786127b15ee7d7
Enderson Santos <enderson.santos.wf@gmail.com>
2022-07-27 14:09:39 +0100

    adding to different files

--- new_new.py
+++ new_new.py
@@ -1,3 +1,13 @@
-import numpy as np
+import json
+def train(as_of_date, countries, categories):
+    args=locals()
+    #args["as_of_date"] == "as_of_date_value"
+    #args["countries"] == "countries_value"
+    #args["categories"] == "categories_value"
+    print(args)
+    args = json.dumps(args)
+    print(args)
+ 
 
-a = np.array([1,2,3])
+if __name__ == "__main__":
+    train("03/08/1999","Brazil","man")
